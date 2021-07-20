2f000897a8b6a036deb2611190b8cadd0bf02562
kprohaszka <cc.kprohaszka@gmail.com>
2021-07-20 14:45:14 +0200

    feat: Add wall collision checking functionality to move method

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
@@ -14,9 +14,11 @@ public abstract class Actor implements Drawable {
 
     public void move(int dx, int dy) {
         Cell nextCell = cell.getNeighbor(dx, dy);
-        cell.setActor(null);
-        nextCell.setActor(this);
-        cell = nextCell;
+        if(!checkCollisionWithWall(nextCell)) {
+            cell.setActor(null);
+            nextCell.setActor(this);
+            cell = nextCell;
+        }
     }
 
     public boolean checkCollisionWithWall(Cell nextCell){

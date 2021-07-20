3570a8cb9433862c615a31bb156c629c91b783d1
azarath1 <ar2oww@gmail.com>
2021-07-20 13:14:01 +0200

    feature: add health and attack damage points to Player

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -5,6 +5,8 @@ import com.codecool.dungeoncrawl.logic.Cell;
 public class Player extends Actor {
     public Player(Cell cell) {
         super(cell);
+        this.setHealth((int) (Math.random() * 10 + 5));
+        this.setDamage(5);
     }
 
     public String getTileName() {

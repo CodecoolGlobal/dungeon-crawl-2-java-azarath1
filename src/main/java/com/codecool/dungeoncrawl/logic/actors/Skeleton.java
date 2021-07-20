d3eadef342dd3b165aad6f8c5e87d262af62b35b
azarath1 <ar2oww@gmail.com>
2021-07-20 13:16:50 +0200

    feature: add health and attack damage points to Skeleton

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
@@ -5,6 +5,8 @@ import com.codecool.dungeoncrawl.logic.Cell;
 public class Skeleton extends Actor {
     public Skeleton(Cell cell) {
         super(cell);
+        this.setDamage((int) (Math.random() * 2 + 2));
+        this.setHealth((int) (Math.random() * 5 + 3));
     }
 
     @Override

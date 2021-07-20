e7af338ffeace62500738ae00dddfdc834f9d8a7
azarath1 <ar2oww@gmail.com>
2021-07-20 12:52:19 +0200

    feature: created setter for health attribute

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
@@ -24,6 +24,10 @@ public abstract class Actor implements Drawable {
         return health;
     }
 
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
     public Cell getCell() {
         return cell;
     }

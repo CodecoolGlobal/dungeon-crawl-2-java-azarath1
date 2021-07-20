44f4371a4a796f8db1816c2c5f3d86c628da0b1d
azarath1 <ar2oww@gmail.com>
2021-07-20 11:30:19 +0200

    feature: created damage int variable to declare damage

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
@@ -6,6 +6,7 @@ import com.codecool.dungeoncrawl.logic.Drawable;
 public abstract class Actor implements Drawable {
     private Cell cell;
     private int health = 10;
+    private int damage = 5;
 
     public Actor(Cell cell) {
         this.cell = cell;

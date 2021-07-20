3e7c5bcc962d9d97bc3f21a5540d19ba5d7e0759
kprohaszka <cc.kprohaszka@gmail.com>
2021-07-20 15:51:30 +0200

    feat: Create checkCollisionWithMonster method

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
@@ -3,6 +3,8 @@ package com.codecool.dungeoncrawl.logic.actors;
 import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.Drawable;
 
+import java.util.List;
+
 public abstract class Actor implements Drawable {
     private Cell cell;
     private int health = 10;
@@ -23,6 +25,16 @@ public abstract class Actor implements Drawable {
 
     public boolean checkCollisionWithWall(Cell nextCell){
         return nextCell.getTileName().equals("wall");
+    }
+
+    public boolean checkCollisionWithMonster(Cell nextCell){
+        List<String> monsters = List.of("spider", "ghost", "skeleton");
+        if(nextCell.getActor() != null){
+            String actorType = nextCell.getActor().getTileName();
+            System.out.println(nextCell.getActor().getTileName());
+            return monsters.contains(actorType);
+        }
+        return false;
     }
 
     public int getHealth() {

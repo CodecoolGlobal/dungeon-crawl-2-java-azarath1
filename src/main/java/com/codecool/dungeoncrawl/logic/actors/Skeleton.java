1d6d3710e43f728dd2b93f65910ee3faac6704ab
azarath1 <ar2oww@gmail.com>
2021-07-21 14:21:54 +0200

    feat: Skeletons can now walking randomly

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
@@ -18,13 +18,20 @@ public class Skeleton extends Actor {
 
     @Override
     public void update() {
+        boolean attackDone = false;
         int[][] directions = { {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
         for (int[] direction : directions) {
             Actor target = getCell().getNeighbor(direction[0], direction[1]).getActor();
             if (target != null && target.getClass() == Player.class) {
+                attackDone = true;
                 attack(direction[0], direction[1]);
                 break;
             }
+        }
+        if (!attackDone) {
+            int[] targetPosition = directions[(int)(Math.random() * 4)];
+            Cell target = getCell().getNeighbor(targetPosition[0], targetPosition[1]);
+            if (target.getActor() == null && target.getType() == CellType.FLOOR) move(targetPosition[0], targetPosition[1]);
         }
     }
 }

1d88dd7b1263ef639fd25fdaeb527eef0a5dab1b
azarath1 <ar2oww@gmail.com>
2021-07-21 14:18:41 +0200

    feat: Skeletons can retaliate from now on

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
@@ -1,6 +1,7 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.CellType;
 
 public class Skeleton extends Actor {
     public Skeleton(Cell cell) {
@@ -13,5 +14,17 @@ public class Skeleton extends Actor {
     @Override
     public String getTileName() {
         return "skeleton";
+    }
+
+    @Override
+    public void update() {
+        int[][] directions = { {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+        for (int[] direction : directions) {
+            Actor target = getCell().getNeighbor(direction[0], direction[1]).getActor();
+            if (target != null && target.getClass() == Player.class) {
+                attack(direction[0], direction[1]);
+                break;
+            }
+        }
     }
 }

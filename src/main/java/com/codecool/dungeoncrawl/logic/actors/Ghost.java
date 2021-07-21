e1915fc5002dafc6096be86219a25932316b2b59
kprohaszka <cc.kprohaszka@gmail.com>
2021-07-21 16:53:13 +0200

    feat: Create movement for ghost

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Ghost.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Ghost.java
@@ -1,6 +1,7 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.CellType;
 
 public class Ghost extends Actor {
     public Ghost(Cell cell) {
@@ -13,5 +14,26 @@ public class Ghost extends Actor {
     @Override
     public String getTileName() {
         return "ghost";
+    }
+
+    @Override
+    public void update() {
+        boolean attackDone = false;
+        int[][] directions = { {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+        for (int[] direction : directions) {
+            Actor target = getCell().getNeighbor(direction[0], direction[1]).getActor();
+            if (target != null && target.getClass() == Player.class) {
+                attackDone = true;
+                attack(direction[0], direction[1]);
+                break;
+            }
+        }
+        if (!attackDone) {
+            int[] targetPosition = directions[(int)(Math.random() * 4)];
+            Cell target = getCell().getNeighbor(targetPosition[0], targetPosition[1]);
+            if (target.getActor() == null &&
+                    (target.getType() == CellType.FLOOR || target.getType() == CellType.WALL ||target.getType() == CellType.EMPTY)) move(targetPosition[0], targetPosition[1]);
+                    System.out.println(target.getType());
+        }
     }
 }

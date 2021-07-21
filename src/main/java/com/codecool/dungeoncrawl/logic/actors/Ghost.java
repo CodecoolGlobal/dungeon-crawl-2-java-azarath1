fb423e68fb0e652201c8523a1fc60f237ebcefba
azarath1 <ar2oww@gmail.com>
2021-07-21 10:35:08 +0200

    feat: set Ghost as enemy

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Ghost.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Ghost.java
@@ -5,6 +5,7 @@ import com.codecool.dungeoncrawl.logic.Cell;
 public class Ghost extends Actor {
     public Ghost(Cell cell) {
         super(cell);
+        this.setEnemy();
         this.setDamage((int) (Math.random() * 4 + 2));
         this.setHealth((int) (Math.random() * 6 + 3));
     }

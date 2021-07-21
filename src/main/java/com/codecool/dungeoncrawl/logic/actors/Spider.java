01b06b8d3de68db5169053ae4c18229c7471c61f
azarath1 <ar2oww@gmail.com>
2021-07-21 10:34:06 +0200

    feat: set Spider as enemy

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Spider.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Spider.java
@@ -5,6 +5,7 @@ import com.codecool.dungeoncrawl.logic.Cell;
 public class Spider extends Actor {
     public Spider(Cell cell) {
         super(cell);
+        this.setEnemy();
         this.setDamage((int) (Math.random() * 3 + 2));
         this.setHealth((int) (Math.random() * 8 + 4));
     }

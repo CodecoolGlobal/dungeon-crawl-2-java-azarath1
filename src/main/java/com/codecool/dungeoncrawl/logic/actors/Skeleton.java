b8918198852719ee99ad84741eaf1b384214119a
azarath1 <ar2oww@gmail.com>
2021-07-21 10:34:34 +0200

    feat: set Skeleton as enemy

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
@@ -5,6 +5,7 @@ import com.codecool.dungeoncrawl.logic.Cell;
 public class Skeleton extends Actor {
     public Skeleton(Cell cell) {
         super(cell);
+        this.setEnemy();
         this.setDamage((int) (Math.random() * 2 + 2));
         this.setHealth((int) (Math.random() * 5 + 3));
     }

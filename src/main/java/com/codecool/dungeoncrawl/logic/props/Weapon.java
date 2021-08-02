f88e192d2fec683a1dc7d3c10d243efce6e3d8a1
NGergely7712 <nagy.gergely19991105@gmail.com>
2021-08-03 00:25:59 +0200

    fix: Fix player not reciving bonus damage when a sword is equiped

--- src/main/java/com/codecool/dungeoncrawl/logic/props/Weapon.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/props/Weapon.java
@@ -3,14 +3,17 @@ package com.codecool.dungeoncrawl.logic.props;
 import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.actors.Actor;
 
+import java.util.Random;
+
 public class Weapon extends Items {
     private String name;
     private int Damage;
+    Random random = new Random();
 
     public Weapon(Cell cell, String name) {
         super(cell);
         setName(name);
-
+        setDamage(random.nextInt(10));
     }
 
     public int getDamage() {

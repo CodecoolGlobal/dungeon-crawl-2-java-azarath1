2e07ddc2df5655bc58cdd8aa33c90a77ff36b073
kprohaszka <cc.kprohaszka@gmail.com>
2021-08-03 09:54:32 +0200

    Create alternative map loader to save game

--- src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
@@ -104,4 +104,78 @@ public class MapLoader {
         return map;
     }
 
+    public static GameMap loadMap(Player player) {
+        //TODO: fix PLAYER losing privilege of cheat mode and inventory after map change
+        String[] maps = {"/map.txt", "/map3.txt"};
+        InputStream is = MapLoader.class.getResourceAsStream(maps[counter]);
+        counter++;
+        Scanner scanner = new Scanner(is);
+        int width = scanner.nextInt();
+        int height = scanner.nextInt();
+
+        scanner.nextLine(); // empty line
+
+        GameMap map = new GameMap(width, height, CellType.EMPTY);
+        for (int y = 0; y < height; y++) {
+            String line = scanner.nextLine();
+            for (int x = 0; x < width; x++) {
+                if (x < line.length()) {
+                    Cell cell = map.getCell(x, y);
+                    switch (line.charAt(x)) {
+                        case 'f':
+                            cell.setType(CellType.FENCE);
+                            break;
+                        case ' ':
+                            cell.setType(CellType.EMPTY);
+                            break;
+                        case '#':
+                            cell.setType(CellType.WALL);
+                            break;
+                        case '.':
+                            cell.setType(CellType.FLOOR);
+                            break;
+                        case 's':
+                            cell.setType(CellType.FLOOR);
+                            new Skeleton(cell);
+                            break;
+                        case 'p':
+                            cell.setType(CellType.FLOOR);
+                            new Spider(cell);
+                            break;
+                        case 'g':
+                            cell.setType(CellType.FLOOR);
+                            new Ghost(cell);
+                            break;
+                        case 'n':
+                            cell.setType(CellType.FLOOR);
+                            new Necromancer(cell);
+                            break;
+                        case '@':
+                            cell.setType(CellType.FLOOR);
+                            player.setCell(cell);
+                            map.setPlayer(player);
+                            break;
+                        case 'e':
+                            cell.setType(CellType.STAIRS);
+                            break;
+                        case 'w':
+                            cell.setType(CellType.WEAPON);
+                            new Weapon(cell,"Sword");
+                            break;
+                        case 'k':
+                            cell.setType(CellType.KEY);
+                            new Key(cell,"Key");
+                            break;
+                        case 'd':
+                            cell.setType(CellType.DOOR);
+                            new door(cell);
+                            break;
+                        default:
+                            throw new RuntimeException("Unrecognized character: '" + line.charAt(x) + "'");
+                    }
+                }
+            }
+        }
+        return map;
+    }
 }

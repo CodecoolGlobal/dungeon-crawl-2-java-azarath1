b2295315a60db0c7cd2b81816d165b343f0485ad
azarath1 <ar2oww@gmail.com>
2021-08-04 15:42:32 +0200

    feat: GameStateDaoJdbc query, constructor

--- src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
@@ -9,8 +9,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameStateDaoJdbc implements GameStateDao {
+    private DataSource dataSource;
+
+    public GameStateDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
-    public void add(GameState state) {
+    public void add(GameState state) throws SQLException {
+        Connection conn = this.dataSource.getConnection();
+        String query = "INSERT INTO game_state(current_map, saved_at, player_id) VALUES (?, ?, ?)";
+        PreparedStatement ps = conn.prepareStatement(query);
+        ps.setString(1, state.getCurrentMap());
+        ps.setDate(2, new Date(System.currentTimeMillis()));
+        ps.setInt(3, state.getPlayer().getId());
+        ps.executeUpdate();
 
     }
 

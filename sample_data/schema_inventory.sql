aa176af250af82eebc4f55cb50c3f0dc2c559f2d
kprohaszka <cc.kprohaszka@gmail.com>
2021-08-04 11:04:14 +0200

    feat: Add inventory table schema

---
+++ sample_data/schema_inventory.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS public.inventory;
+CREATE TABLE inventory (
+   id serial NOT NULL PRIMARY KEY,
+   player_id integer NOT NULL,
+   item_name text NOT NULL,
+   object_name text NOT NULL,
+   amount integer NOT NULL
+);
+
+ALTER TABLE ONLY public.inventory
+    ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);

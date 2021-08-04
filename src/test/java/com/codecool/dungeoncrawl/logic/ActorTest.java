bf0b79688a80d38fc1fc786a0469cb5b7a7da84c
azarath1 <ar2oww@gmail.com>
2021-08-04 08:12:45 +0200

    refactor: actor unit tests + fix: cannotMoveIntoWall(), add: setup()

--- src/test/java/com/codecool/dungeoncrawl/logic/ActorTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/ActorTest.java
@@ -2,18 +2,25 @@ package com.codecool.dungeoncrawl.logic;
 
 import com.codecool.dungeoncrawl.logic.actors.Player;
 import com.codecool.dungeoncrawl.logic.actors.Skeleton;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ActorTest {
-    GameMap gameMap = new GameMap(3, 3, CellType.FLOOR);
+    GameMap gameMap;
+    Player player;
+
+
+    @BeforeEach
+    void setUp() {
+        gameMap = new GameMap(3, 3, CellType.FLOOR);
+    }
 
     @Test
     void moveUpdatesCells() {
         Player player = new Player(gameMap.getCell(1, 1));
         player.move(1, 0);
-
         assertEquals(2, player.getX());
         assertEquals(1, player.getY());
         assertEquals(null, gameMap.getCell(1, 1).getActor());
@@ -22,10 +29,9 @@ class ActorTest {
 
     @Test
     void cannotMoveIntoWall() {
-        gameMap.getCell(2, 1).setType(CellType.WALL);
         Player player = new Player(gameMap.getCell(1, 1));
+        gameMap.getCell(2, 1).setType(CellType.WALL);
         player.move(1, 0);
-
         assertEquals(1, player.getX());
         assertEquals(1, player.getY());
     }
@@ -33,8 +39,6 @@ class ActorTest {
     @Test
     void cannotMoveOutOfMap() {
         Player player = new Player(gameMap.getCell(2, 1));
-        player.move(1, 0);
-
         assertEquals(2, player.getX());
         assertEquals(1, player.getY());
     }
@@ -44,11 +48,12 @@ class ActorTest {
         Player player = new Player(gameMap.getCell(1, 1));
         Skeleton skeleton = new Skeleton(gameMap.getCell(2, 1));
         player.move(1, 0);
-
         assertEquals(1, player.getX());
         assertEquals(1, player.getY());
         assertEquals(2, skeleton.getX());
         assertEquals(1, skeleton.getY());
         assertEquals(skeleton, gameMap.getCell(2, 1).getActor());
     }
+
+
 }

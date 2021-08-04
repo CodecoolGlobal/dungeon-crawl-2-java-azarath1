8a01ee2701363507c29a67c72338c49fb1a7e536
kprohaszka <cc.kprohaszka@gmail.com>
2021-08-04 11:12:52 +0200

    feat: Modify schema

--- sample_data/schema_inventory.sql
+++ sample_data/schema_inventory.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS public.inventory;
-CREATE TABLE inventory (
+CREATE TABLE public.inventory (
    id serial NOT NULL PRIMARY KEY,
    player_id integer NOT NULL,
    item_name text NOT NULL,

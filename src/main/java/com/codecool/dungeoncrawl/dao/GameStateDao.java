66d3f7790b8a8e540cefc5f459e336d04425ba0b
azarath1 <ar2oww@gmail.com>
2021-08-04 15:41:36 +0200

    fix: GameStateDao SQLException

--- src/main/java/com/codecool/dungeoncrawl/dao/GameStateDao.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameStateDao.java
@@ -2,10 +2,11 @@ package com.codecool.dungeoncrawl.dao;
 
 import com.codecool.dungeoncrawl.model.GameState;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface GameStateDao {
-    void add(GameState state);
+    void add(GameState state) throws SQLException;
     void update(GameState state);
     GameState get(int id);
     List<GameState> getAll();
